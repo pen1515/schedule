e5d0ab6f0e499acb793e03044f912f62fb3618ce
pen1515 <k21007kk@aitech.ac.jp>
2023-09-04 22:58:08 +0900

    データベース追加

---
+++ initdb/create_database.sql
@@ -0,0 +1,10 @@
+create database dataa;
+
+-- -- use pengin;
+
+-- -- CREATE TABLE IF NOT EXISTS testtest (
+-- --     id INT PRIMARY KEY,
+-- --     name VARCHAR(50),
+-- --     email VARCHAR(100),
+-- --     certification VARCHAR(100)
+-- -- ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
